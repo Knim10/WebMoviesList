a55041c712dd9136ced66b9a50ebeaa9c87edab2
Kenneth Nimmo <knimmo1@dmacc.edu>
2023-10-06 11:53:38 -0500

    Class Stubs

---
+++ src/main/java/helpers/MovieHelper.java
@@ -0,0 +1,17 @@
+package helpers;
+
+/**
+ * @author Kenneth Nimmo - Knimmo
+ * CIS175 - Fall 2021
+ * Oct 6, 2023
+ */
+public class MovieHelper {
+
+	/**
+	 * 
+	 */
+	public MovieHelper() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/main/java/model/Movie.java
@@ -0,0 +1,17 @@
+package model;
+
+/**
+ * @author Kenneth Nimmo - Knimmo
+ * CIS175 - Fall 2021
+ * Oct 6, 2023
+ */
+public class Movie {
+
+	/**
+	 * 
+	 */
+	public Movie() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
